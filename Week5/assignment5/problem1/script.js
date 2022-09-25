d76b54bc77ef42c25ea81d5ccbddc0c13a0b97f1
ALuke24 <alukens@edgewood.edu>
2022-09-25 16:31:46 -0500

    assignment 5

---
+++ Week5/assignment5/problem1/script.js
@@ -0,0 +1,3 @@
+function addText(){
+    $("#appendDiv").append("<p class='inputP'>" + document.getElementById("input").value + "</p>");
+}
---
+++ Week5/assignment5/problem2/script.js
@@ -0,0 +1,7 @@
+function changeDiv(){
+    $("#top_of_page").css("background-color", "#778C91");
+    $("#top_of_page").css("width", "800px");
+    $("#top_of_page").css("height", "400px");
+    console.log("testing");
+}
+
